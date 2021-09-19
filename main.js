6791829729302eb851d01bd652728387f46c5ebc
matheusboliveir <theus.braz18@gmail.com>
2021-09-19 01:06:45 -0300

    setting webPreferences to use require in renderer

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
-const { app,BrowserWindow,screen } = require('electron');
+const { app, BrowserWindow, screen } = require('electron');
 
-require('electron-reload')(__dirname,{
+require('electron-reload')(__dirname, {
     electron: require(`${__dirname}/node_modules/electron`)
 });
 
 let win;
 
 app.on('ready', () => {
-   const {width, height} = screen.getPrimaryDisplay().workAreaSize;
-   win = new BrowserWindow({width, height});
-   win.removeMenu();
-   win.loadFile('app/html/index.html');
+    const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+    win = new BrowserWindow({
+        width,
+        height,
+        autoHideMenuBar: true,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
+    });
+    //win.removeMenu();
+    win.loadFile('app/index.html');
 });
+
+app.on('window-all-closed', () => {
+    return app.quit();
+  });
